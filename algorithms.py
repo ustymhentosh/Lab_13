c7125f70efc299dc8091749c5593e6cdffad63d6
Ustym Hentosh <116552529+ustymhentosh@users.noreply.github.com>
2022-12-16 16:10:05 +0200

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,14 @@
- def linear_search(list_of_values, value):
-    pass
+ def linear_search(array,value):
+    """
+    (list, int) -> int
+    Function finds first index of value. If value is not in array, function will return -1
+    >>> linear_search([1,2,3,414,1424,1241,24,124,124,1,412,4,],1)
+    0
+    """
+    for i in range(len(array)):
+        if array[i]==value:
+            return i
+    return -1
   
   
   
