3e7ae480b4f1870cc2aa803385ce9cc0f4f995b0
Ustym Hentosh <116552529+ustymhentosh@users.noreply.github.com>
2022-12-16 16:15:35 +0200

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -8,15 +8,28 @@ def merge_sort(lst):
 
   
   
-  def binary_search(list_of_values, value):
+def binary_search(list_of_values, value):
     pass
   
   
   
-  def selection_sort(lst):
-    pass
+def selection_sort(lst):
+    '''
+    Sorts a list with an algoritm of selection.
+    >>> selection_sort([2, 4, 3, 1])
+    [1, 2, 3, 4]
+    >>> selection_sort([10000000, 6567, 0, 4567, 78])
+    [0, 78, 4567, 6567, 10000000]
+    '''
+    for i in range(len(lst)):
+        min_i = i
+        for j in range(i+1, len(lst)):
+            if lst[j] < lst[min_i]:
+                min_i = j
+        lst[min_i], lst[i] = lst[i], lst[min_i]
+    return lst
   
   
   
-  def quick_sort(lst):
-    pass
+def quick_sort(lst):
+   pass
