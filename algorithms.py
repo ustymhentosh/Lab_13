4f87e376f339c6c04274dc980c2ddfb446a6d2c0
Ustym Hentosh <116552529+ustymhentosh@users.noreply.github.com>
2022-12-16 16:14:02 +0200

    Create algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,22 @@
+def linear_search(array,value):
+   pass
+  
+ 
+  
+def merge_sort(lst):
+    pass
+
+  
+  
+  def binary_search(list_of_values, value):
+    pass
+  
+  
+  
+  def selection_sort(lst):
+    pass
+  
+  
+  
+  def quick_sort(lst):
+    pass
